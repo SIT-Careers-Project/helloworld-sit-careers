1f027fdf68694c866b30fccd93168a329aa87c0c
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2020-10-20 15:42:04 +0700

    Create student Seeder 🐯

---
+++ back-end/database/seeds/StudentSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('students')->insert([
+            'first_name' => 'Poy',
+            'last_name' => 'EiEi',
+        ]);
+    }
+}
