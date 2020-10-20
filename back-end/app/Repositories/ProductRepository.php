693f91f0d6f42181cbfb6d36efac14842cefe792
kaotu <kao-tu@hotmail.co.th>
2020-10-20 23:25:38 +0700

    🍻 Create product to database 🌚

--- back-end/app/Repositories/ProductRepository.php
+++ back-end/app/Repositories/ProductRepository.php
@@ -6,4 +6,12 @@ class ProductRepository implements ProductRepositoryInterface
     public function getProduct(){
         return Product::all();
     }
+
+    public function createProduct($create_product){
+        $product = Product::insertGetId([
+            'product_name' => $create_product['product_name'], 
+            'product_price' => $create_product['product_price']
+        ]);
+        return $product;
+    }
 }
--- back-end/app/Repositories/ProductRepositoryInterface.php
+++ back-end/app/Repositories/ProductRepositoryInterface.php
@@ -4,4 +4,5 @@ namespace App\Repositories;
 interface ProductRepositoryInterface
 {
     public function getProduct();
+    public function createProduct($create_product);
 }
